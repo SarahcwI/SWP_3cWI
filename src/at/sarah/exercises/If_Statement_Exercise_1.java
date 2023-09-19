f2e585f7338291ffd2a43b7be46b42dce764b4e8
Sarah <sarah.wolf@htldornbirn.at>
2023-09-19 09:15:28 +0200

    Finished_Exercise_1

---
+++ src/at/sarah/exercises/If_Statement_Exercise_1.java
@@ -0,0 +1,27 @@
+package at.sarah.exercises;
+
+import java.util.Random;
+
+public class    If_Statement_Exercise_1 {
+    public static void main(String[] args) {
+
+        Random random = new Random();
+        int randomNumber = random.nextInt(100);
+
+        System.out.println(randomNumber);
+
+        if (randomNumber < 20){
+            System.out.println("Mini");
+        }
+
+        if (randomNumber > 20 && randomNumber < 50){
+            System.out.println("Medium");
+        }
+
+        if (randomNumber > 50){
+            System.out.println("Large");
+        }
+
+
+    }
+}
