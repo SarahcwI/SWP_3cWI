f83096260cb8483656233682560fb35d810b0fa4
Sarah <sarah.wolf@htldornbirn.at>
2023-09-22 08:42:39 +0200

    Finished_Exercise_4

---
+++ src/at/sarah/exercises/Loops1_Exercise_4.java
@@ -0,0 +1,11 @@
+package at.sarah.exercises;
+
+public class Loops1_Exercise_4 {
+    public static void main(String[] args) {
+        int memory = 0;
+        for (int i = 0; i <= 100; i++){
+            memory += i;
+            System.out.println(memory);
+        }
+    }
+}
