6a485e4400050ac66fbd74e4c42302da9d1cc80f
Sarah <sarah.wolf@htldornbirn.at>
2023-10-18 09:32:55 +0200

    Methods_working_finally

---
+++ src/at/sarah/ExerciseCeaserEncryption/CeaserEncryption.java
@@ -0,0 +1,20 @@
+package at.sarah.ExerciseCeaserEncryption;
+
+
+public class CeaserEncryption {
+
+    public static void main(String[] args) {
+        int a = 5;
+        System.out.println(Encryption.encryption(a));
+
+    }
+
+
+
+
+
+}
+
+
+/*Bei der Cäsar-Verschlüsselung werden die Buchstaben um eine fixe Anzahl Stellen verschoben (z.B. 2) a wird zu c, f wird zu h, z wird zu b usw.
+Implementiere eine Methode encrypt(String):String und eine Methode decrypt(String):String*/
---
+++ src/at/sarah/ExerciseCeaserEncryption/Encryption.java
@@ -0,0 +1,12 @@
+package at.sarah.ExerciseCeaserEncryption;
+
+public class Encryption {
+    public static int encryption(int data){
+        int b = 4;
+        int result = data * b;
+        System.out.println(result);
+        return result;
+    }
+
+
+}
