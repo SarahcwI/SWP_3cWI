96a43f4a83b82d96d7e5265d40d1e2a2c73903d2
Sarah <sarah.wolf@htldornbirn.at>
2023-11-21 23:43:58 +0100

    converting_to_*_working

--- src/at/sarah/WordGuessing/wordGuesser.java
+++ src/at/sarah/WordGuessing/wordGuesser.java
@@ -1,16 +1,23 @@
 package at.sarah.WordGuessing;
-
-import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.Random;
 
 public class wordGuesser {
     public static void main(String[] args) {
         String[] wordsToGuess = {"Alfredo", "Johannes", "Jurgen", "Tafel"};
-
         Random random = new Random();
-        int randomNumber = random.nextInt(wordsToGuess.length);
+        int indexOfWordToGuess = random.nextInt(wordsToGuess.length);
+        System.out.println(wordsToGuess[indexOfWordToGuess]);
 
-        System.out.println(wordsToGuess[randomNumber]);
+        char[] eachLetter = wordsToGuess[indexOfWordToGuess].toCharArray();
+        char[] output = Arrays.copyOf(eachLetter, eachLetter.length);
+        for (int converter = 0; converter < eachLetter.length; converter++) {
+
+            output[converter] = '*';
+
+        }
+        System.out.println(output);
+
     }
 }
+
