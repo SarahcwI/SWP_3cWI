365c2967058559215097189e9da4aa84e3a2a6d9
Sarah <sarah.wolf@htldornbirn.at>
2024-02-21 08:46:46 +0100

    added_Lamp

---
+++ src/at/sarah/oo/Lamp/Lamp.java
@@ -0,0 +1,14 @@
+package at.sarah.oo.Lamp;
+
+import at.sarah.oo.carExercise.RearMirror;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lamp {
+    private List<Glowelement> glowelements;
+
+    public Lamp() {
+        this.glowelements = new ArrayList<>();
+    }
+}
