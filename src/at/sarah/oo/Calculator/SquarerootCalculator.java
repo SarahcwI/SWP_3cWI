8ea4abb3dfa6f5c1adff2536d18f3956c6acd4d6
Sarah <sarah.wolf@htldornbirn.at>
2024-02-26 17:42:09 +0100

    added_sqrtCalc_and_finished_exercise

---
+++ src/at/sarah/oo/Calculator/SquarerootCalculator.java
@@ -0,0 +1,7 @@
+package at.sarah.oo.Calculator;
+
+public class SquarerootCalculator extends BaseCalculator{
+    public double squareRoot(double number){
+        return Math.sqrt(number);
+    }
+}
--- src/at/sarah/oo/Calculator/main.java
+++ src/at/sarah/oo/Calculator/main.java
@@ -5,6 +5,8 @@ public class main {
     public static void main(String[] args) {
         BaseCalculator bc1 = new BaseCalculator();
         ScientificCalculator sc1 = new ScientificCalculator();
+        SquarerootCalculator sqrt1 = new SquarerootCalculator();
+
 
         //bc1 add
         System.out.println(bc1.add(4,6));
@@ -22,6 +24,10 @@ public class main {
         //sc1 add
         System.out.println(sc1.add(4,5));
 
+        //sqrt1 squareroot
+        System.out.println(sqrt1.squareRoot(9));
+        //sqrt1 add
+        System.out.println(sqrt1.add(5,8));
 
 
     }
