2f98a988d2ca96e1b10727e48e1fcc1fe5b7bbc7
Sarah <sarah.wolf@htldornbirn.at>
2024-02-26 17:31:52 +0100

    added_sientific_Calc

--- src/at/sarah/oo/Calculator/ScientificCalculator.java
+++ src/at/sarah/oo/Calculator/ScientificCalculator.java
@@ -1,2 +1,13 @@
-package at.sarah.oo.Calculator;public class ScientificCalculator {
+package at.sarah.oo.Calculator;
+
+public class ScientificCalculator extends BaseCalculator{
+    public double sin(double a){
+        double rad = Math.toRadians(a);
+        return Math.sin(rad);
+    }
+
+    public double cos(double b){
+        double rad = Math.toRadians(b);
+        return Math.cos(rad);
+    }
 }
--- src/at/sarah/oo/Calculator/main.java
+++ src/at/sarah/oo/Calculator/main.java
@@ -4,8 +4,23 @@ public class main {
 
     public static void main(String[] args) {
         BaseCalculator bc1 = new BaseCalculator();
-        System.out.println(bc1.add(4,6));
+        ScientificCalculator sc1 = new ScientificCalculator();
 
+        //bc1 add
+        System.out.println(bc1.add(4,6));
+        //bc1 subtract
+        System.out.println(bc1.subtract(4,6));
+        //bc1 multiply
+        System.out.println(bc1.multiply(4,6));
+        // bc1 divide
         System.out.println(bc1.divide(10,4));
+
+        //sc1 sin
+        System.out.println(sc1.sin(50));
+        //sc1 cos
+        System.out.println(sc1.cos(50));
+
+
+
     }
 }
